1bcfcbd98752561bc6e654e19cc72712d8e3541d
이상은 <dltahazh@gmail.com>
2017-08-12 00:34:49 +0900

    Post(body-parser)

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/form', function(req, res) {
+  res.render('form');
+});
 
-app.get('/topic', function(req, res) {
+app.get('/form_receiver', function(req, res) {
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title + ',' + description);
+});
+
+app.post('/form_receiver', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title + ',' + description);
+});
+app.get('/topic/:id', function(req, res) {
   var topics = [
     'Javascript is....',
     'Nodejs is....',
     'Express is....'
   ];
   var output = `
-    <a href="/topic?id=0">Javascript</a><br>
-    <a href="/topic?id=1">Nodejs</a><br>
-    <a href="/topic?id=2">Express</a><br><br>
-    ${topics[req.query.id]}
+    <a href="/topic/0">Javascript</a><br>
+    <a href="/topic/1">Nodejs</a><br>
+    <a href="/topic/2">Express</a><br><br>
+    ${topics[req.params.id]}
   `
   res.send(output);
 })
