7c356d26659799d9d09d0983481b0c907dfb17be
이상은 <dltahazh@gmail.com>
2017-08-11 21:48:28 +0900

    jade template

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.get('/template', function(req, res) {
+  res.render('temp', {_title:"hi"});
+});
 app.get('/', function(req, res) {
   res.send('Hello');
 })
