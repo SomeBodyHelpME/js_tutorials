5a18109ba149d294ef3a4d17a154916efe72efa6
이상은 <dltahazh@gmail.com>
2017-08-11 23:45:40 +0900

    query string

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
+
+app.get('/topic', function(req, res) {
+  var topics = [
+    'Javascript is....',
+    'Nodejs is....',
+    'Express is....'
+  ];
+  var output = `
+    <a href="/topic?id=0">Javascript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">Express</a><br><br>
+    ${topics[req.query.id]}
+  `
+  res.send(output);
+})
 app.get('/template', function(req, res) {
   res.render('temp', {_title:"hi"});
 });
